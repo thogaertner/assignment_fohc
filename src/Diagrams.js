fc91470745be641242d8b18bf256e4e195fb1362
tommysft <thgaertner.sft@t-online.de>
2019-01-09 18:24:38 +0100

    AmChart works!! :) but is not sorted

--- src/Diagrams.js
+++ src/Diagrams.js
@@ -3,236 +3,236 @@ import { Col } from 'react-bootstrap';
 import './App.css';
 import HousingDefectDiagram from './HousingDefectDiagram';
 import AmBarChart from './AmBarChart'
+import VariableBar2 from './VariableBar_2';
 import VariableBar from './VariableBar';
-
 
 class Diagrams extends Component {
     render() {
         const data = this.props.data;
         const selected = this.props.selected;
         const displaySelection = this.props.displaySelection;
-        return <div> 
-            <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={displaySelection}/>
+        return <div>
+            <Col md={12}>
+                <VariableBar2
+                    data={data}
+                    selected={selected}
+                    displaySelection={displaySelection}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Housing_Defects'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Housing_Defects'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Airquality_rate'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Airquality_rate'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Tobaccoretail_rate'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Tobaccoretail_rate'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Supermarketarea_rate'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Supermarketarea_rate'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Poverty'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Poverty'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Unemployment'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Unemployment'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Rent_burden'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Rent_burden'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Preterm_Births'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Preterm_Births'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Teen_Births'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Teen_Births'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Teen_Births'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Teen_Births'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Schoolabsent_rate'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Schoolabsent_rate'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Jailincarceration'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Jailincarceration'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Assault_Hosps'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Assault_Hosps'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Self_rep_health'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Self_rep_health'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Smoking'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Smoking'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Sugary_Drink'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Sugary_Drink'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Fruit_Veg'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Fruit_Veg'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Obesity'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Obesity'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Diabetes'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Diabetes'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Alc_Hosp'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Alc_Hosp'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Drug_Hosp'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Drug_Hosp'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Insurance'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Insurance'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Didnt_Get_Care'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Didnt_Get_Care'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Late_No_Prenatal'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Late_No_Prenatal'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'HPV_vaccination'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'HPV_vaccination'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Flu_Vaccination'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Flu_Vaccination'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'HIV_Testing_Ever'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'HIV_Testing_Ever'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'HIV_Diagnosis'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'HIV_Diagnosis'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Stroke_Hosp'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Stroke_Hosp'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Psych_Hosp'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Psych_Hosp'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Asthma_5to14'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Asthma_5to14'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Avoidable_Asthma'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Avoidable_Asthma'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Avoidable_Diabetes'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Avoidable_Diabetes'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'IMR'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'IMR'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Premature_Mort'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Premature_Mort'}/>
             </Col>
             <Col md={4}>
-            <VariableBar
-                data={data}
-                selected={selected}
-                displaySelection={'Life_expectancy_rate'}/>
+                <VariableBar
+                    data={data}
+                    selected={selected}
+                    displaySelection={'Life_expectancy_rate'}/>
             </Col>
         </div>
     }
---
+++ src/VariableBar_2.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { rgba } from 'polished';
+import { BarChart } from 'react-easy-chart';
+import './App.css';
+import { getColorById } from './utils';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+
+
+class VariableBar2 extends Component {
+    state = {}
+    transformedData = {};
+    selected;
+
+    componentDidMount() {
+        let chart = am4core.create("displaySelection", am4charts.XYChart);
+        chart.scrollbarX = new am4core.Scrollbar();
+
+        // Add data
+        chart.data = this.data;
+
+        // Create axes
+
+        let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "Name";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 30;
+        categoryAxis.renderer.labels.template.horizontalCenter = "right";
+        categoryAxis.renderer.labels.template.verticalCenter = "middle";
+        categoryAxis.renderer.labels.template.rotation = 270;
+        categoryAxis.tooltip.disabled = true;
+        categoryAxis.renderer.minHeight = 110;
+
+        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+        // Create series
+        let series = chart.series.push(new am4charts.ColumnSeries());
+        series.dataFields.valueY = this.displaySelection;
+        series.dataFields.categoryX = "Name";
+        series.name = this.displaySelection;
+        series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+        series.columns.template.fillOpacity = .8;
+
+
+        let columnTemplate = series.columns.template;
+        columnTemplate.strokeWidth = 0;
+        columnTemplate.fill = am4core.color("gray");
+
+
+        // on hover, make corner radiuses bigger
+        let hoverState = series.columns.template.column.states.create("hover");
+        hoverState.properties.cornerRadiusTopLeft = 0;
+        hoverState.properties.cornerRadiusTopRight = 0;
+        hoverState.properties.fillOpacity = 1;
+
+        series.columns.template.adapter.add("fill", (fill, target)=>{
+            return this.colorData(this.data[target.dataItem.index].ID);
+       })
+
+        this.chart = chart;
+    }
+
+
+
+    componentWillUnmount() {
+        if (this.chart) {
+            this.chart.dispose();
+        }
+    }
+
+    render() {
+
+        this.data = this.props.data;
+        this.selected = this.props.selected;
+        this.displaySelection = this.props.displaySelection;
+
+        if (this.chart) {
+            this.componentDidMount();
+        }
+
+        return <div>
+            {this.displaySelection.split('_')
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(' ')}
+            <div id="displaySelection" style={{ width: "800", height: "500px" }}></div>
+        </div>
+    }
+
+    colorData(item){
+        if (this.selected.indexOf(item) === -1 )  {
+            return am4core.color('gray');
+        }
+        return am4core.color(getColorById(item));
+    }
+
+    transformData() {
+        if (this.transformedData[this.displaySelection]) return this.transformedData[this.displaySelection];
+
+        this.transformedData[this.displaySelection] = this.data.map(date => {
+            return {
+                x: date['Name'],
+                y: date[this.displaySelection],
+                id: date['ID']
+            }
+        })
+        this.transformedData[this.displaySelection].sort((a, b) => a.y-b.y)
+
+        return this.transformedData[this.displaySelection];
+    }
+
+
+}
+export default VariableBar2;
